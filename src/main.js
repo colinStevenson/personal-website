0ada24fbf8c6c197f48d8c63c8527e8637e50cf3
Colin Stevenson <colin.stevenson@gmail.com>
2017-01-16 12:45:27 -0700

    Removes vuex store, currently a store is not needed

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App';
-import store from './store';
 import routes from './routes';
 
 const router = new VueRouter({
@@ -17,5 +16,4 @@ new Vue({
     template: '<App/>',
     components: { App },
     router,
-    store,
 })
