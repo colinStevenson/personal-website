aaf7838df49f160bfcf24c82138ed38a7134df85
Colin Stevenson <colin.stevenson@gmail.com>
2017-01-28 14:11:01 -0700

    Sets active tab for photo and photoset pages

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,9 +5,19 @@
 				<router-link :to="{ name:'home', params: { intro: true }}">Colin Stevenson</router-link>
 			</h1>
 			<ul class="navbar-right nav navbar-nav">
-				<li><router-link :to="{ name: 'home', params: { intro: false }}">Photography</router-link></li>
+				<li :class="{active: isPhotographyRoute }"><router-link :to="{ name: 'home', params: { intro: false }}">Photography</router-link></li>
 				<li><router-link :to="{ name: 'about'}">About</router-link></li>
 			</ul>
 		</div>
 	</header>
 </template>
+<script>
+	const PHOTOGRAPY_ROUTES = ["home", "photoset", "photo"];
+	export default {
+		computed: {
+			isPhotographyRoute(){
+				return PHOTOGRAPY_ROUTES.includes(this.$route.name);
+			},
+		},
+	}
+</script>
