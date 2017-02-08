d75e7b43e1901ac43443a205ff8b740b508e4103
Colin Stevenson <colin.stevenson@gmail.com>
2017-02-07 20:34:38 -0700

    Removes Photo route

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,6 @@ import Photoset from './components/routes/Photoset.vue';
 const routes = [
 	{ name: 'about', path: '/about', component: About },
 	{ name: 'home', path: '/home', component: Home },
-	{ name: 'photo', path: '/photo/:id', component: Photo },
 	{ name: 'photoset', path: '/photoset/:id', component: Photoset },
 	{ path: '*', redirect: '/home' },
 ];
